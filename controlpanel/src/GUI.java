fdc3e06d9caf16555e4c925aee1b0e1c9af91a3e
user.email <elizajadefury@gmail.com>
2020-04-23 18:25:28 +1000

    JTextField prints to terminal, working on making it print to screen

--- controlpanel/src/GUI.java
+++ controlpanel/src/GUI.java
@@ -11,9 +11,11 @@ public class GUI  {
     public static void main(String args[]) {
 
         //Creating the Frame
+        int Width = 900;
+        int Height = 700;
         JFrame frame = new JFrame("Control Panel Review");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(2000, 800);
+        frame.setSize(Width, Height);
 
         //Creating the MenuBar and adding components
         /*Across tab */
@@ -32,23 +34,35 @@ public class GUI  {
         /*Under file */
         JMenuItem m11 = new JMenuItem("Import");
         JMenuItem m22 = new JMenuItem("Export");
-        JMenuItem m33 = new JMenuItem("Save");
+        JMenuItem m33 = new JMenuItem("Billboards");
+        JMenuItem m44 = new JMenuItem("Schedule and Save");
         m1.add(m11);
         m1.add(m22);
         m1.add(m33);
+        m1.add(m44);
 
         //Creating the panel at bottom and adding components
         JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton submit = new JButton("Submit");
+        //Appears next to JTextField
+        JLabel label = new JLabel("Type text and press enter");
+        JTextField tf = new JTextField(10); // accepts up to 10 characters
+        //Don't think text button is needed if TextField submits as you type to label
+        //JButton submit = new JButton("Submit");
         /*Color button */
         JButton color = new JButton("Color");
         panel.add(label); // Components Added using Flow Layout
         panel.add(tf);
-        panel.add(submit);
         panel.add(color);
 
+        //When you enter text, it submits to terminal
+        //Working on printing to screen
+        String testing = tf.getText();
+        tf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("The entered text is: " + tf.getText());
+            }
+        });
 
 
         // Text Area at the Center
@@ -56,9 +70,10 @@ public class GUI  {
         //String message = "Welcome to today's fundraiser!";
 
         //int FRAME_WIDTH = 2000;
-
+        //Sorting out the ratio
         String message = "Welcome to today's fundraiser!";
         String info = "Please take your seats in the ballroom. Seats are assigned, so please make use of the seating plan";
+        //String info = "Please take your seats in the ballroom. Seats are assigned, so please make use of the seating plan";
 
         JLabel LTitle = new JLabel(message, JLabel.CENTER);
         JLabel LInfo = new JLabel(info, JLabel.CENTER);
@@ -68,18 +83,18 @@ public class GUI  {
         //frame.setSize(FRAME_WIDTH, 700);
 
         Dimension dim = frame.getSize();
-        int x = dim.width;
+        Width = dim.width;
         //int x = 1000;
-        int y = dim.height;
+        Height = dim.height;
 
-        LTitle.setSize(x, y);
+        LTitle.setSize(Width, Height);
         Font TitleFont = LTitle.getFont();
         int size = TitleFont.getSize();
         int width = LTitle.getFontMetrics(TitleFont).stringWidth(message);
         System.out.println("Width :" + width);
-        System.out.println("x :" + x);
+        System.out.println("x :" + Width);
 
-        while(width<x-10){
+        while(width<Width-10){
             size++;
             LTitle.setFont(new Font("Serif", Font.PLAIN, size));
             width = LTitle.getFontMetrics(LTitle.getFont()).stringWidth(message);
@@ -99,14 +114,7 @@ public class GUI  {
         frame.setVisible(true);
 
 
-       //Class doesn't work
-       class ClickListener implements ActionListener{
-           public void actionPerformed(ActionEvent e)
-           {
-               if(e.getSource() == submit)
-                   submit.setText("ta");
-           }
-       }
+
     }
 
 }
