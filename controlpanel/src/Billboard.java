5787ece13d38436d7e60b2529d00e8085c948776
user.email <elizajadefury@gmail.com>
2020-05-04 14:37:14 +1000

    Very basic Gui representing a list

--- controlpanel/src/Billboard.java
+++ controlpanel/src/Billboard.java
@@ -1,4 +1,35 @@
-public class Billboard {
+// java Program to create a simple JList
+import javax.swing.*;
+public class Billboard
+{
+
+    Billboard() {
+        //create a new frame
+        JFrame billboardframe = new JFrame("frame");
+        JPanel listPanel =new JPanel();
+        JLabel billboardList = new JLabel("select a test");
+        String test[]= { "1","2"};
+        JList listInstance= new JList(test);
+        listInstance.setSelectedIndex(2);
+        listPanel.add(listInstance);
+        billboardframe.add(listPanel);
+        billboardframe.setSize(400,400);
+        billboardframe.setVisible(true);
+    }
+
+    //main class
+    public static void main(String[] args) {
+        new Billboard();
+    }
+
+
+}
+
+
+
+
+
+/*public class Billboard {
     private String BName;
     private String BOwner;
     private boolean scheduled;
@@ -35,4 +66,4 @@ public class Billboard {
         scheduled = s;
     }
 
-}
+}*/
