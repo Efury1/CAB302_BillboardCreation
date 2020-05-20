6c085d5a7943d286bd1a7fc6e39db29292b26991
user.email <elizajadefury@gmail.com>
2020-05-21 09:14:25 +1000

    Basic data for users, billboard and schedule.

--- src/server/server.sql
+++ src/server/server.sql
@@ -22,4 +22,24 @@ CREATE TABLE  IF NOT EXISTS `cab302`.`Users` (
   `PermissionEditBillboards` int(1) NOT NULL,
   `PermissionSchedule` int(1) NOT NULL,
   PRIMARY KEY  (`Username`)
+)
+
+CREATE TABLE  IF NOT EXISTS `cab302`.`Billboards` (
+  `BillboardID` int(10) NOT NULL AUTO_INCREMENT,
+  `MessageText` varchar(45),
+  `MessageColor` varchar(45),
+  `InformationText` varchar(45),
+  `InformationColor` varchar(45),
+  `BackgroundColor` varchar(45),
+  `PictureURL` varchar(175),
+  `PictureData` varchar(175),
+  PRIMARY KEY  (`BillboardID`)
+)
+
+CREATE TABLE  IF NOT EXISTS `cab302`.`Schedules` (
+  `ScheduleID` int(10) NOT NULL AUTO_INCREMENT,
+  `StartTime` datetime NOT NULL,
+  `EndTime` datetime NOT NULL,
+  `BillboardID` int(10) NOT NULL,
+  PRIMARY KEY  (`Username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
