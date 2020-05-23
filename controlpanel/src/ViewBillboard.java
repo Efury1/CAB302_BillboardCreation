43b1e5b164e9ff7c5e3df9acf7e7e94358d1f5f7
Lauren Howlett <lauren.howlett@connect.qut.edu.au>
2020-05-23 10:16:55 +1000

    Added method to create JPanels in correct ratio for billboards with message and image only

--- controlpanel/src/ViewBillboard.java
+++ controlpanel/src/ViewBillboard.java
@@ -4,7 +4,8 @@ import java.awt.*;
 public class ViewBillboard {
     public static void main(String args[]) {
         //MessageAndInfo();
-        MessageImageInfo();
+        //MessageImageInfo();
+        MessageAndImage();
     }
 
     public static void MessageAndInfo(){
@@ -71,6 +72,50 @@ public class ViewBillboard {
         frame.add(infoPanel);
 
         frame.setLayout(new GridLayout(3,1));
+
+        frame.pack();
+        frame.setSize(1000, 700);
+        frame.setVisible(true);
+    }
+
+    public static void MessageAndImage(){
+        JFrame frame = new JFrame("View Billboard");
+        frame.setLayout(new GridBagLayout());
+        frame.setSize(1000, 700);
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel messagePanel = new JPanel();
+        gbc.weighty = 0.33;
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.VERTICAL;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        messagePanel.setLayout(new GridBagLayout());
+
+
+        JLabel message = new JLabel("Message", JLabel.CENTER);
+        messagePanel.add(message);
+
+        frame.add(messagePanel, gbc);
+
+
+
+        JPanel imagePanel = new JPanel();
+        gbc.weighty = 0.66;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        imagePanel.setLayout(new GridBagLayout());
+
+        JLabel image = new JLabel("Insert image here", JLabel.CENTER);
+        imagePanel.add(image);
+
+
+        frame.add(imagePanel, gbc);
+
 
         frame.pack();
         frame.setSize(1000, 700);
