872734782b5c4b09e93fef7f4c7ea455a6c440a8
Lauren Howlett <lauren.howlett@connect.qut.edu.au>
2020-05-26 19:40:00 +1000

    Created function to return creator and billboard name as an ArrayList of Strings as well as code to parse this ArrayList

--- server/src/Connect.java
+++ server/src/Connect.java
@@ -4,17 +4,55 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Connect {
     public static void main (String[] args) throws SQLException {
+        Connection myConnection = DBConnection.getInstance();
+        Statement myStatement = myConnection.createStatement();
+        //populateDB();
+        ArrayList<String> data = new ArrayList<String>();
+        data = getBillboards();
 
+
+       for(int i = 0; i<data.size(); i++){
+            System.out.println("Creator: " + data.get(i));
+            i++;
+            System.out.println("Billboard name: " + data.get(i));
+            System.out.println();
+        }
+        myConnection.close();
+
+    }
+    public static ArrayList<String> getBillboards() throws SQLException{
+        ArrayList<String> billboardData = new ArrayList<String>();
+
+        Connection myConnection = DBConnection.getInstance();
+        Statement myStatement = myConnection.createStatement();
+
+        ResultSet billboardResults = myStatement.executeQuery("SELECT username, billboard_name FROM billboard_database.user_billboards;");
+
+        while(billboardResults.next()){
+            String bOwner = billboardResults.getString("username");
+            billboardData.add(bOwner);
+            String bName = billboardResults.getString("billboard_name");
+            billboardData.add(bName);
+
+            //System.out.println(bName + "   " + bOwner);
+        }
+
+        myConnection.close();
+        return billboardData;
+    }
+
+    public static void connectTest() throws SQLException{
         Connection myConnection = DBConnection.getInstance();
 
         Statement myStatement = myConnection.createStatement();
 
-      //"FOREIGN KEY (billboard_ID) REFERENCES billboards(billboard_ID))");
+        //"FOREIGN KEY (billboard_ID) REFERENCES billboards(billboard_ID))");
 
-       // myStatement.executeQuery("INSERT INTO billboard_database.billboards (billboard_name, title) VALUES ('HelloWorldBillboard', 'Hello World')");
+        // myStatement.executeQuery("INSERT INTO billboard_database.billboards (billboard_name, title) VALUES ('HelloWorldBillboard', 'Hello World')");
         //myStatement.executeQuery("INSERT INTO billboard_database.user_billboards (username, billboard_name) VALUES ('fudog', 'HelloWorldBillboard')");
 
         ResultSet myResults = myStatement.executeQuery("SELECT * FROM billboard_database.users;");
@@ -49,4 +87,19 @@ public class Connect {
         //  Close the connection
         myConnection.close();
     }
+
+    public static void populateDB() throws SQLException{
+        Connection myConnection = DBConnection.getInstance();
+
+        Statement myStatement = myConnection.createStatement();
+
+        myStatement.executeQuery("INSERT INTO billboard_database.billboards (billboard_name, title) VALUES ('NewBillboard', 'Welcome')");
+        myStatement.executeQuery("INSERT INTO billboard_database.user_billboards (username, billboard_name) VALUES ('sunpark', 'NewBillboard')");
+
+        myStatement.executeQuery("INSERT INTO billboard_database.billboards (billboard_name, title) VALUES ('BirthdayMessage', 'Happy Birthday!')");
+        myStatement.executeQuery("INSERT INTO billboard_database.user_billboards (username, billboard_name) VALUES ('superuser', 'BirthdayMessage')");
+
+        myConnection.close();
+    }
+
 }
