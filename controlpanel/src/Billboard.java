c315b6fe7c8c10f148e54c9034870eebef7e894d
Lauren Howlett <lauren.howlett@connect.qut.edu.au>
2020-05-26 19:04:16 +1000

    Removed unused code

--- controlpanel/src/Billboard.java
+++ controlpanel/src/Billboard.java
@@ -1,36 +1,3 @@
-// java Program to create a simple JList
-/*import javax.swing.*;
-import java.awt.*;
-public class Billboard extends JFrame
-
-{
-    JScrollPane scrollpane;
-
-    public Billboard()
-    {
-        super("Billboard Viewer");
-        setSize(300, 200);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        String categories[] = {"Example 1", "Example 2", "Example 3","Example 4", "Example 5", "Example 6", "Example 7", "Example 8", "Example 9" };
-        JList billboardList = new JList(categories);
-        scrollpane = new JScrollPane(billboardList);
-        getContentPane().add(scrollpane, BorderLayout.CENTER);
-    }
-
-    // Main Method 
-    public static void main(String args[])
-    {
-        Billboard session1 = new Billboard();
-        session1.setVisible(true);
-    }
-} */
-
-
-
-
-
-
-
 public class Billboard {
     private String BName;
     private String BOwner;
