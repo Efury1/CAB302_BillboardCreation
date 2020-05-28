6982dbd25ef1a35a8b836de88d4988d40d63299d
user.email <elizajadefury@gmail.com>
2020-05-28 10:09:51 +1000

    Set up basic UnitTesting class for Login. I have written the possible negative and positive scenarios.

---
+++ controlpanel/src/UnitTestingLogin.java
@@ -0,0 +1,63 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/*All possible scenarios
+* 1. Client tries to connect to database. Success is printed
+* to terminal.
+* 2. Client tries to connect to database but server failed.
+* An error is received and user is prompted to try again.
+* 1.Username and password fields are empty, "Enter" is click.
+* and an error dialog appears
+* 2.If username and password fields are invalid, the 'Enter' is pressed.
+* An error dialog appears. */
+
+public class UnitTestingLogin {
+    @Test
+    public void testConnectInvalid()
+    {
+        //Login using default user
+        //should return correct vlue
+        //assertNotNull();
+
+    }
+
+    @Test
+    public void testConnectValid()
+    {
+        //Login using default user
+        //should return correct vlue
+        //assertNotNull();
+
+    }
+
+    @Test
+    public void testUserPasswordEmpty()
+    {
+        //Login using default user
+        //should return correct vlue
+        //assertNotNull();
+
+    }
+
+    @Test
+    public void testUserPasswordInvalid()
+    {
+        //Login using default user
+        //should return correct vlue
+        //assertNotNull();
+
+    }
+
+    @Test
+    public void testUserPasswordValid()
+    {
+        //Login using default user
+        //should return correct vlue
+        //assertNotNull();
+
+    }
+
+
+}
