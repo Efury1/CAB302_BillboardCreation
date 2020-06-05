585e2799d7607cee1dc972df231d768658f531b9
Aden720 <aden3600@gmail.com>
2020-06-05 22:53:56 +1000

    Minor changes to viewer

--- controlpanel/src/BillboardViewer.java
+++ controlpanel/src/BillboardViewer.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.text.View;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.sql.SQLException;
@@ -123,13 +124,16 @@ public class BillboardViewer {
                     }
                 }
             }
-            if(candidateBoardName == "" || candidateBoardOwner == ""){
+            if(candidateBoardName == "" || candidateBoardOwner == ""){ //If no valid billboard was scheduled at this time, show the default
                 ShowDefaultBB();
             } else {
-                Billboard billboardToShow = new Billboard(candidateBoardName, candidateBoardOwner);
+                Billboard billboardToShow = new Billboard(candidateBoardName, candidateBoardOwner); //Create and get the info of the board to display
                 try {
-                    billboardToShow.importAllInfo();
+                    billboardToShow.importAllInfo(); //Get the billboards data from the server
                     ViewBillboard.showBillboard(billboardToShow);
+                    ViewBillboard.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                    ViewBillboard.frame.setUndecorated(true);
+                    ViewBillboard.frame.setVisible(true);
                 } catch (IOException | ClassNotFoundException | SQLException e) {
                     ShowDefaultBB();
                 }
@@ -159,6 +163,9 @@ public class BillboardViewer {
         defaultBB.setBMessageColour("0000FF");
         try {
             ViewBillboard.showBillboard(defaultBB);
+            ViewBillboard.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            ViewBillboard.frame.setUndecorated(true);
+            ViewBillboard.frame.setVisible(true);
         } catch (IOException | SQLException e) {
             System.err.println("Could not display default Billboard: " + e);
         }
